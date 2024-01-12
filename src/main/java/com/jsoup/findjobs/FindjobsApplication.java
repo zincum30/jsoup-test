bfb7d557e7bccde2e6fa470a92baa450bd1ef3bd
practise30 <practise30@gmail.com>
2024-01-13 08:54:13 +0900

    initialize

---
+++ src/main/java/com/jsoup/findjobs/FindjobsApplication.java
@@ -0,0 +1,13 @@
+package com.jsoup.findjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindjobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindjobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsoup/findjobs/FindjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsoup.findjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindjobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
