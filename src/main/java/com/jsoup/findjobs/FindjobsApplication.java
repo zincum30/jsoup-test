058218575279300301c594dcefb5b035a7edff67
practise30 <practise30@gmail.com>
2024-05-13 09:36:22 +0900

    feat : 외부 API 적용

--- src/main/java/com/jsoup/findjobs/FindjobsApplication.java
+++ src/main/java/com/jsoup/findjobs/FindjobsApplication.java
@@ -1,13 +1,24 @@
 package com.jsoup.findjobs;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class FindjobsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FindjobsApplication.class, args);
-	}
+	public static void main(String[] args) throws Exception {
 
+		String url = "https://www.wanted.co.kr/wd/142762";
+		Document document = Jsoup.connect(url).get();
+		Elements elements = document.getElementsByClass(".JobContent_descriptionWrapper__SM4UD");
+
+		for (Element element : elements) {
+			System.out.println(element.text());
+		}
+
+	}
 }
